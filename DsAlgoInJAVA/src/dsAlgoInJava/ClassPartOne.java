a1907b49f4dbbce3ddf2056eaa3a1306238b206c
Pallavi Das <pallavids92@gmail.com>
2021-07-10 22:47:37 +0530

    java set up

---
+++ DsAlgoInJAVA/src/dsAlgoInJava/ClassPartOne.java
@@ -0,0 +1,16 @@
+package dsAlgoInJava;
+
+public class ClassPartOne {
+
+	// template for creating object
+	//user define type
+	
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
